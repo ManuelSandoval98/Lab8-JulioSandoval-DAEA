dd84070c7e692d6ca8d563f4a05538e5afaa8c75
ManuelSandoval98 <julio.sandoval.l@tecsup.edu.pe>
2025-06-16 16:49:30 -0500

    Add Dockerfile for .NET 9 deployment on Render3

--- Lab8-JulioSandoval/Models/LinqExampleContext.cs
+++ Lab8-JulioSandoval/Models/LinqExampleContext.cs
@@ -20,9 +20,8 @@ public partial class LinqExampleContext : DbContext
 
     public virtual DbSet<product> products { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=linqexample;Username=postgres;Password=manuel1234");
+    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //=> optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=linqexample;Username=postgres;Password=manuel1234");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
